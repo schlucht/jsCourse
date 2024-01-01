e321c72449919c435b96f6f8f367fcc43d19d158
Schlucht <web@jagolo.ch>
2024-01-01 17:55:53 +0100

    Teil 1 beendet

---
+++ machineLearning/web/js/sketchPad.js
@@ -0,0 +1,83 @@
+class SketchPad {
+  constructor(container, size = 400) {
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = size;
+    this.canvas.height = size;
+    this.canvas.style = `
+      background-color: white;
+      box-shadow: 0px 0px 10px 2px black;
+    `;
+    container.appendChild(this.canvas);
+    const lineBreak = document.createElement('br');
+    container.appendChild(lineBreak);
+
+    this.btnUndo = document.createElement('button');
+    this.btnUndo.innerHTML = 'LÖSCHEN';
+    //this.btnUndo.disabled = 'false';
+    container.appendChild(this.btnUndo);
+
+    this.ctx = this.canvas.getContext('2d');
+
+    this.reset();
+    this.#addEventListener();
+  }
+
+  reset() {
+    this.paths = [];
+    this.isDrawing = false;
+    this.#redraw();
+  }
+
+  #addEventListener() {
+    this.canvas.onmousedown = (evt) => {
+      const mouse = this.#getMouse(evt);
+      this.paths.push([mouse]);
+      this.isDrawing = true;
+    };
+
+    this.canvas.onmousemove = (evt) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouse(evt);
+        const lastPath = this.paths[this.paths.length - 1];
+        lastPath.push(mouse);
+        this.#redraw();
+      }
+    };
+    document.onmouseup = () => {
+      this.isDrawing = false;
+    };
+    this.canvas.ontouchstart = (evt) => {
+      const loc = evt.touches[0];
+      this.canvas.onmousedown(loc);
+    };
+    this.canvas.ontouchmove = (evt) => {
+      const loc = evt.touches[0];
+      this.canvas.onmousemove(loc);
+    };
+    document.ontouchen = () => {
+      document.onmouseup(loc);
+    };
+    this.btnUndo.onclick = () => {
+      this.paths.pop();
+      this.#redraw();
+    }
+  }
+
+  #redraw() {
+    this.ctx.clearRect(0, 0, this.canvas.height, this.canvas.width);
+    draw.paths(this.ctx, this.paths);
+    if (this.paths.length > 0) {
+      this.btnUndo.disabled = false;
+    } else {
+      this.btnUndo.disabled = true;
+    }
+
+  }
+  #getMouse(evt) {
+    const rect = this.canvas.getBoundingClientRect();
+    return [
+      Math.round(evt.clientX - rect.left),
+      Math.round(evt.clientY - rect.top)
+    ];
+  }
+}
